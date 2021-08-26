344f15d6c54809178518ab0868be0236d3c9ef43
rofi <rfigueroa@truelogic.io>
2021-08-26 09:50:42 -0400

    ok

--- packages/dapplib/src/dapp-transactions.js
+++ packages/dapplib/src/dapp-transactions.js
@@ -28,26 +28,6 @@ module.exports = class DappTransactions {
 		`;
 	}
 
-	static create_family() {
-		return fcl.transaction`
-				import RegistryFamilyContract from 0x01cf0e2f2f715450
-				
-				transaction(name: String, price: UFix64) {
-				
-				  var adminRef: &RegistryFamilyContract.Admin
-				
-				  prepare(acct: AuthAccount) {
-				    self.adminRef = acct.borrow<&RegistryFamilyContract.Admin>(from: RegistryFamilyContract.AdminStoragePath) ?? panic("Cannot borrow admin ref")
-				  }
-				
-				  execute {
-				    self.adminRef.createFamily(name: name, price: price)
-				  }
-				}
-				 
-		`;
-	}
-
 	static batch_mint_collectible_from_family() {
 		return fcl.transaction`
 				import RegistryFamilyContract from 0x01cf0e2f2f715450
@@ -71,6 +51,26 @@ module.exports = class DappTransactions {
 				    self.receiverReference.batchDeposit(collection: <-collection)
 				  }
 				}
+		`;
+	}
+
+	static create_family() {
+		return fcl.transaction`
+				import RegistryFamilyContract from 0x01cf0e2f2f715450
+				
+				transaction(name: String, price: UFix64) {
+				
+				  var adminRef: &RegistryFamilyContract.Admin
+				
+				  prepare(acct: AuthAccount) {
+				    self.adminRef = acct.borrow<&RegistryFamilyContract.Admin>(from: RegistryFamilyContract.AdminStoragePath) ?? panic("Cannot borrow admin ref")
+				  }
+				
+				  execute {
+				    self.adminRef.createFamily(name: name, price: price)
+				  }
+				}
+				 
 		`;
 	}
 
@@ -114,6 +114,38 @@ module.exports = class DappTransactions {
 		`;
 	}
 
+	static registry_receive_auth_nft() {
+		return fcl.transaction`
+				import RegistryService from 0x01cf0e2f2f715450
+				
+				// Allows a Tenant to register with the RegistryService. It will
+				// save an AuthNFT to account storage. Once an account
+				// has an AuthNFT, they can then get Tenant Resources from any contract
+				// in the Registry.
+				//
+				// Note that this only ever needs to be called once per Tenant
+				
+				transaction() {
+				
+				    prepare(signer: AuthAccount) {
+				        // if this account doesn't already have an AuthNFT...
+				        if signer.borrow<&RegistryService.AuthNFT>(from: RegistryService.AuthStoragePath) == nil {
+				            // save a new AuthNFT to account storage
+				            signer.save(<-RegistryService.register(), to: RegistryService.AuthStoragePath)
+				
+				            // we only expose the IAuthNFT interface so all this does is allow us to read
+				            // the authID inside the AuthNFT reference
+				            signer.link<&RegistryService.AuthNFT{RegistryService.IAuthNFT}>(RegistryService.AuthPublicPath, target: RegistryService.AuthStoragePath)
+				        }
+				    }
+				
+				    execute {
+				
+				    }
+				}
+		`;
+	}
+
 	static registry_receive_tenant() {
 		return fcl.transaction`
 				import RegistrySampleContract from 0x01cf0e2f2f715450
@@ -153,38 +185,6 @@ module.exports = class DappTransactions {
 				  }
 				}
 				
-		`;
-	}
-
-	static registry_receive_auth_nft() {
-		return fcl.transaction`
-				import RegistryService from 0x01cf0e2f2f715450
-				
-				// Allows a Tenant to register with the RegistryService. It will
-				// save an AuthNFT to account storage. Once an account
-				// has an AuthNFT, they can then get Tenant Resources from any contract
-				// in the Registry.
-				//
-				// Note that this only ever needs to be called once per Tenant
-				
-				transaction() {
-				
-				    prepare(signer: AuthAccount) {
-				        // if this account doesn't already have an AuthNFT...
-				        if signer.borrow<&RegistryService.AuthNFT>(from: RegistryService.AuthStoragePath) == nil {
-				            // save a new AuthNFT to account storage
-				            signer.save(<-RegistryService.register(), to: RegistryService.AuthStoragePath)
-				
-				            // we only expose the IAuthNFT interface so all this does is allow us to read
-				            // the authID inside the AuthNFT reference
-				            signer.link<&RegistryService.AuthNFT{RegistryService.IAuthNFT}>(RegistryService.AuthPublicPath, target: RegistryService.AuthStoragePath)
-				        }
-				    }
-				
-				    execute {
-				
-				    }
-				}
 		`;
 	}
 
